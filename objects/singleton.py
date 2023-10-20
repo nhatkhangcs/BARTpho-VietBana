8c1de705017ef5aed1bdd06e6e83d3097106e78f
Khang Vo <nhatkhangdtp@gmail.com>
2023-10-20 21:22:16 +0700

    singleton.py updated

--- objects/singleton.py
+++ objects/singleton.py
@@ -1,15 +1,5 @@
-# class Singleton(type):
-#     _instances = {}
-
-#     def __call__(cls, *args, **kwargs):
-#         if cls not in cls._instances:
-#             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-#         return cls._instances[cls]
-
-from weakref import WeakValueDictionary
-
 class Singleton(type):
-    _instances = WeakValueDictionary()
+    _instances = {}
 
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
